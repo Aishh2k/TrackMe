84e7e40ebc2701f1b1d17fe8f489a70c68a70811
aishh2k <aiswarya2000may@gmail.com>
2021-04-05 12:55:26 -0700

    Responsive

--- src/Components/JSX/Main.jsx
+++ src/Components/JSX/Main.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../CSS/Main.css'
 import {MapContainer, TileLayer, Marker } from 'react-leaflet'
 import L from 'leaflet';
-import { Col, Row , Container } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import {  ReactComponent as Arrow } from '../Images/icon-arrow.svg';
 
 const publicIp = require('public-ip');
@@ -81,7 +81,7 @@ class Main extends Component {
               lng:data.data.location.lng,
               ip: data.data.ip,
               isp:data.data.isp,
-              time:data.data.location.timezone,
+              time: "UTC" + " " + data.data.location.timezone,
               loc:data.data.location.city + ", " +data.data.location.region + ", " + data.data.location.country  ,
               position:[data.data.location.lat, data.data.location.lng]
               
@@ -101,9 +101,18 @@ class Main extends Component {
         
     render() {
         return (
-            <div>
-                <Container  className="displaybox " >
-                    <Row className="rowstyle" style={{paddingBottom:"20px"}}>
+            <div id="main-div" >
+                <div id="navbar">
+
+                    <h2>IP Address Tracker</h2>
+                    <form  type="submit" onSubmit={ this.handleSubmit }>
+                        <input type="text" placeholder="Search for any IP address or domain" value={this.state.ipaddress} onChange={this.handleChange} />
+                        <button>< Arrow/></button> 
+                    </form>
+                </div>
+
+                <div className="displaybox" >
+                    <Row className="rowstyle" style={{paddingBottom:"20px",}}>
                         <Col md  className="cols bods ">
                             <p>IP ADDRESS</p>
                             <h4>{this.state.ip}</h4>
@@ -116,7 +125,7 @@ class Main extends Component {
 
                         <Col md className="cols bods">
                             <p>TIMEZONE</p>
-                            <h4>UTC {this.state.time}</h4>
+                            <h4>{this.state.time}</h4>
                         </Col>
 
                         <Col md style={{marginRight:"10px"}} className="cols">
@@ -124,20 +133,10 @@ class Main extends Component {
                             <h4>{this.state.isp}</h4>
                         </Col>
                     </Row>
-                </Container>
-
-
-                <div id="navbar">
-
-                    <h2>IP Address Tracker</h2>
-                    <form  type="submit" onSubmit={ this.handleSubmit }>
-                        <input type="text" placeholder="Search for any IP address or domain" value={this.state.ipaddress} onChange={this.handleChange} />
-                        <button>< Arrow/></button> 
-                    </form>
                 </div>
 
 
-                <MapContainer center={{ lat: 51.505, lng: -0.09 }} zoom={5} whenCreated={map => this.setState({ map })}>
+                <MapContainer id="maps" center={{ lat: 51.505, lng: -0.09 }} zoom={5} whenCreated={map => this.setState({ map })}>
                     <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
